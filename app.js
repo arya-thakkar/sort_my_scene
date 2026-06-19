2f732e4bff1e892b3b225dda756b7083037b86b6
arya-thakkar <a.thakkar071006@gmail.com>
2026-06-19 14:34:45 +0530

    fix: set main to server.js and explicitly allow wildcard CORS

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
